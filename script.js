bbca33d91a41f8d65b2225596d345024720f6053
bapt-v <bapt-v@live.fr>
2016-08-04 16:20:13 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -239,48 +239,44 @@ function searchTerms(){
 
 window.onload=function(){
 
-	$.support.cors = true;
-	$.mobile.allowCrossDomainPages = true;
+	// $.support.cors = true;
+	// $.mobile.allowCrossDomainPages = true;
 
-//apparition du header
-	// Hide Header on on scroll down
-	var didScroll;
-	var lastScrollTop = 0;
-	var delta = 5;
-	var navbarHeight = $('header').outerHeight();
-	//console.log(navbarHeight);
-	$(window).scroll(function(event){
-		didScroll = true;
-	});
 
-	setInterval(function() {
-		if (didScroll) {
-			hasScrolled();
-			didScroll = false;
-		}
-	}, 250);
+	// var didScroll;
+	// var lastScrollTop = 0;
+	// var delta = 5;
+	// var navbarHeight = $('header').outerHeight();
 
-	function hasScrolled() {
-		var st = $(this).scrollTop();
+	// $(window).scroll(function(event){
+		// didScroll = true;
+	// });
+
+	// setInterval(function() {
+		// if (didScroll) {
+			// hasScrolled();
+			// didScroll = false;
+		// }
+	// }, 250);
+
+	// function hasScrolled() {
+		// var st = $(this).scrollTop();
 		
-		// Make sure they scroll more than delta
-		if(Math.abs(lastScrollTop - st) <= delta)
-			return;
+
+		// if(Math.abs(lastScrollTop - st) <= delta)
+			// return;
 		
-		// If they scrolled down and are past the navbar, add class .nav-up.
-		// This is necessary so you never see what is "behind" the navbar.
-		if (st > lastScrollTop && st > navbarHeight){
-			// Scroll Down
-			$('header').removeClass('nav-down').addClass('nav-up');
-		} else {
-			// Scroll Up
-			if(st + $(window).height() < $(document).height()) {
-				$('header').removeClass('nav-up').addClass('nav-down');
-			}
-		}
+
+		// if (st > lastScrollTop && st > navbarHeight){
+			// $('header').removeClass('nav-down').addClass('nav-up');
+		// } else {
+			// if(st + $(window).height() < $(document).height()) {
+				// $('header').removeClass('nav-up').addClass('nav-down');
+			// }
+		// }
 		
-		lastScrollTop = st;
-	}
+		// lastScrollTop = st;
+	// }
 // fin apparition du header
 
 	setArticlesList(currentArticlesList)
